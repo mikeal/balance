967d5597c34950a5086eb60d3d08e02d15856fba
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-03-22 22:55:21 -0700

    Initial commit.

---
+++ test/run.js
@@ -0,0 +1,12 @@
+var http = require('http'),
+    sys = require('sys'),
+    balance = require('../lib/balance');
+
+var b = new balance.Balancer();
+b.addListener("route", function (request) {
+  request.setRoute(5984, 'localhost')
+})
+
+var server = b.getServer();
+server.listen(8000);
+sys.puts("Server running at http://127.0.0.1:8000/")
